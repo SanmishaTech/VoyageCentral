5cb93688863d3548bcad954227e9119d78048019
Ganesh-Ghadi <ganeshghadi084@gmail.com>
2025-04-05 11:03:22 +0530

    c

--- src/config/email.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  user: process.env.EMAIL_USER,
-  password: process.env.EMAIL_PASSWORD,
-  from: process.env.EMAIL_FROM,
-};
--- src/config/permissions.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'roles.read': ['admin'],
-  'users.read': ['admin', 'user'],
-  'users.write': ['admin'],
-  'users.delete': ['admin'],
-  'users.export': ['admin'],
-};
