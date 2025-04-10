7cfb4c715bab7ed33ff072ea7072fb6870a67cd5
Vipulkoli2000 <vipulkoli2323@gmail.com>
2025-04-10 14:43:14 +0530

    sidebar

--- src/components/common/app-sidebar.tsx
+++ src/components/common/app-sidebar.tsx
@@ -88,6 +88,10 @@ const initialData = {
           title: "City",
           url: "#",
         },
+        {
+          title: "Branches",
+          url: "/branches",
+        },
       ],
     },
   ],
