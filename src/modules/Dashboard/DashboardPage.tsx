946b35831ceec132dba0fe9635381f9656616404
Vipulkoli2000 <vipulkoli2323@gmail.com>
2025-04-12 12:07:50 +0530

    Dashboard applied better skeletons

--- src/modules/Dashboard/DashboardPage.tsx
+++ src/modules/Dashboard/DashboardPage.tsx
@@ -1,29 +1,31 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 const DashboardPage = () => {
   return (
     <>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl skeleton" />
-          <div className="aspect-video rounded-xl skeleton" />
-          <div className="aspect-video rounded-xl skeleton" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl skeleton md:min-h-min" />
+        <Skeleton className="min-h-[100vh] flex-1 rounded-xl bg-gray-200 dark:bg-slate-900 md:min-h-min" />
       </div>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl skeleton" />
-          <div className="aspect-video rounded-xl skeleton" />
-          <div className="aspect-video rounded-xl skeleton" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl skeleton md:min-h-min" />
+        <Skeleton className="min-h-[100vh] flex-1 rounded-xl bg-gray-200 dark:bg-slate-900 md:min-h-min" />
       </div>
       <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
         <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-          <div className="aspect-video rounded-xl skeleton" />
-          <div className="aspect-video rounded-xl skeleton" />
-          <div className="aspect-video rounded-xl skeleton" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
+          <Skeleton className="aspect-video rounded-xl bg-gray-200 dark:bg-slate-900" />
         </div>
-        <div className="min-h-[100vh] flex-1 rounded-xl skeleton md:min-h-min" />
+        <Skeleton className="min-h-[100vh] flex-1 rounded-xl bg-gray-200 dark:bg-slate-900 md:min-h-min" />
       </div>
     </>
   );
